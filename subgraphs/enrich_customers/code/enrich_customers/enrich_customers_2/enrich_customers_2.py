143538f97730d0b89103eea60a661e71e297ab57
several27@icloud.com <several27@icloud.com>
2022-10-24 22:22:47 +0000

    🔁 Modified     - Subgraph        - enrich_customers     - Pipeline        - mrr_reporting

--- subgraphs/enrich_customers/code/enrich_customers/enrich_customers_2/enrich_customers_2.py
+++ subgraphs/enrich_customers/code/enrich_customers/enrich_customers_2/enrich_customers_2.py
@@ -5,9 +5,9 @@ from prophecy.utils import *
 from . import *
 
 def enrich_customers_2(spark: SparkSession, in0: DataFrame) -> DataFrame:
-    df_regions = regions(spark)
     df_customer_nations = customer_nations(spark)
     df_nation = nation(spark)
+    df_regions = regions(spark)
     df_with_nations_regions = with_nations_regions(spark, in0, df_customer_nations, df_nation, df_regions)
     df_cleanup = cleanup(spark, df_with_nations_regions)
 
