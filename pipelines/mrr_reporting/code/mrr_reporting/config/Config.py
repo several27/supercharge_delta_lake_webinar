848bb1d8de17ce8cf43fa8598e83f6016587bf6e
several27@icloud.com <several27@icloud.com>
2022-10-27 16:50:15 +0000

    🔁 Modified     - Subgraph        - enrich_customers     - Pipeline        - mrr_reporting

--- pipelines/mrr_reporting/code/mrr_reporting/config/Config.py
+++ pipelines/mrr_reporting/code/mrr_reporting/config/Config.py
@@ -3,9 +3,10 @@ from prophecy.config import ConfigBase
 
 class Config(ConfigBase):
 
-    def __init__(self, ):
+    def __init__(self, filterExpression: str=None):
         self.spark = None
-        self.update()
+        self.update(filterExpression)
 
-    def update(self, ):
+    def update(self, filterExpression: str="in0.c_custkey = 1"):
+        self.filterExpression = filterExpression
         pass
--- subgraphs/enrich_customers/code/enrich_customers/enrich_customers_1/enrich_customers_1.py
+++ subgraphs/enrich_customers/code/enrich_customers/enrich_customers_1/enrich_customers_1.py
@@ -4,10 +4,10 @@ from pyspark.sql.types import *
 from prophecy.utils import *
 from . import *
 
-def enrich_customers_2(spark: SparkSession, in0: DataFrame) -> DataFrame:
-    df_customer_nations = customer_nations(spark)
+def enrich_customers_1(spark: SparkSession, in0: DataFrame) -> DataFrame:
     df_nation = nation(spark)
     df_regions = regions(spark)
+    df_customer_nations = customer_nations(spark)
     df_with_nations_regions = with_nations_regions(spark, in0, df_customer_nations, df_nation, df_regions)
     df_cleanup = cleanup(spark, df_with_nations_regions)
 
