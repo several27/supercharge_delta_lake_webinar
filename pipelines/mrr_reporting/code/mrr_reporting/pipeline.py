a3242f16e6a2112e6fc187324904fa0152e0171b
several27@icloud.com <several27@icloud.com>
2023-04-13 22:17:05 +0000

    🔁 Modified     - Pipeline        - mrr_reporting     - Job        - daily_job     - Subgraph        - enrich_customers

--- pipelines/mrr_reporting/code/mrr_reporting/pipeline.py
+++ pipelines/mrr_reporting/code/mrr_reporting/pipeline.py
@@ -11,8 +11,8 @@ def pipeline(spark: SparkSession) -> None:
     df_silver_customers_2_1 = silver_customers_2_1(spark)
     df_by_customer_id = by_customer_id(spark, df_silver_customers_2_1, df_silvers_orders)
     df_sum_amounts = sum_amounts(spark, df_by_customer_id)
-    df_round_amounts = round_amounts(spark, df_sum_amounts)
-    df_enrich_customers_1 = enrich_customers_1(spark, Config.enrich_customers_1, df_round_amounts)
+    df_clean_amounts = clean_amounts(spark, df_sum_amounts)
+    df_enrich_customers_1 = enrich_customers_1(spark, Config.enrich_customers_1, df_clean_amounts)
     final_report(spark, df_enrich_customers_1)
 
 def main():
