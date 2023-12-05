f7b09812851d11ceefb973566473c29b22a0e508
several27@icloud.com <several27@icloud.com>
2023-12-05 16:21:52 +0000

    🔁 Modified     - Subgraph        - enrich_customers     - Pipeline        - mrr_reporting

--- pipelines/mrr_reporting/code/mrr_reporting/pipeline.py
+++ pipelines/mrr_reporting/code/mrr_reporting/pipeline.py
@@ -8,12 +8,12 @@ from prophecy.utils import *
 from mrr_reporting.graph import *
 
 def pipeline(spark: SparkSession) -> None:
-    df_silvers_orders = silvers_orders(spark)
     df_silver_customers_2_1 = silver_customers_2_1(spark)
+    df_silvers_orders = silvers_orders(spark)
     df_by_customer_id = by_customer_id(spark, df_silver_customers_2_1, df_silvers_orders)
     df_sum_amounts = sum_amounts(spark, df_by_customer_id)
-    df_clear_amounts = clear_amounts(spark, df_sum_amounts)
-    df_enrich_customers = enrich_customers(spark, Config.enrich_customers, df_clear_amounts)
+    df_floor_amounts = floor_amounts(spark, df_sum_amounts)
+    df_enrich_customers = enrich_customers(spark, Config.enrich_customers, df_floor_amounts)
     final_report(spark, df_enrich_customers)
 
 def main():
