fcfaccc2c1ec16aa7f0e4d9b4f310e3b9c0b177c
several27@icloud.com <several27@icloud.com>
2023-12-06 16:10:05 +0000

    🔁 Modified     - Dataset        - customer_nations        - final_report        - nation        - silver_customers        - silver_orders        - regions     - Subgraph        - enrich_customers     - Pipeline        - mrr_reporting

--- pipelines/mrr_reporting/code/mrr_reporting/graph/final_report.py
+++ pipelines/mrr_reporting/code/mrr_reporting/graph/final_report.py
@@ -7,4 +7,12 @@ from mrr_reporting.config.ConfigStore import *
 from mrr_reporting.udfs.UDFs import *
 
 def final_report(spark: SparkSession, in0: DataFrame):
+    from pyspark.sql.utils import AnalysisException
+
+    try:
+        desc_table = spark.sql("describe formatted `lakehouse`.`final_report`")
+        table_exists = True
+    except AnalysisException as e:
+        table_exists = False
+
     in0.write.format("delta").mode("error").saveAsTable("`lakehouse`.`final_report`")
