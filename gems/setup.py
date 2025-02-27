095af938e9ed391c279335b844faf9fad5487add
several27@icloud.com <several27@icloud.com>
2025-02-27 18:27:17 +0000

    [Prophecy] Release version 87

--- gems/setup.py
+++ gems/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 packages_to_include = find_packages(exclude = ['test.*', 'test', 'test_manual'])
 setup(
     name = 'prophecy_webinar_supercharge_delta_lake',
-    version = '86',
+    version = '87',
     packages = packages_to_include,
     description = '',
     install_requires = [],
